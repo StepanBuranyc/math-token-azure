f5b18fe2b60c95414f62d3f2ace82d2a0fa43344
StepanBuranyc <buranyc@gmail.com>
2024-12-12 15:01:18 +0100

    Initial commit with math app and Terraform config

--- main.tf
+++ main.tf
@@ -7,27 +7,24 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 
-resource "azurerm_app_service_plan" "asp" {
-  name                = "math-token-plan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+resource "azurerm_storage_account" "storage" {
+  name                     = "mathtokenstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-resource "azurerm_app_service" "app" {
-  name                = "math-token-app"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+resource "azurerm_storage_container" "web" {
+  name                  = "web"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "blob"
+}
 
-  site_config {
-    always_on = true
-  }
-
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
-  }
+resource "azurerm_storage_blob" "index_html" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = azurerm_storage_container.web.name
+  type                   = "Block"
+  source                 = "${path.module}/web/index.html"
 }
