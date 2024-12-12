9c48db8018e59a9c83b790c06cb448baa810c5a1
StepanBuranyc <buranyc@gmail.com>
2024-12-12 15:34:28 +0100

    Updated Terraform files and GitHub Actions workflow

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
+# Vytvoření Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "math-token-rg"
   location = "East US"
 }
 
+# Vytvoření Storage Account
 resource "azurerm_storage_account" "storage" {
   name                     = "mathtokenstorage"
   resource_group_name      = azurerm_resource_group.rg.name
@@ -15,12 +22,14 @@ resource "azurerm_storage_account" "storage" {
   account_replication_type = "LRS"
 }
 
+# Vytvoření Blob Container
 resource "azurerm_storage_container" "web" {
   name                  = "web"
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "blob"
 }
 
+# Nahrání index.html jako Blob
 resource "azurerm_storage_blob" "index_html" {
   name                   = "index.html"
   storage_account_name   = azurerm_storage_account.storage.name
